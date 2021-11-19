341cc29a1afa79d4a1075546e7c986dc1d0ad963
Yaroslav Shumakov <noiseman2000@mail.ru>
2021-11-19 11:10:51 +0300

    Full example minor fixes

--- examples/cartridge-full/fragments/helpers.lua
+++ examples/cartridge-full/fragments/helpers.lua
@@ -17,7 +17,7 @@ local function fragment()
             insert = { enabled = true, include = {}, exclude = {} },
         })
     end
-    if service_ok == true then
+    if spaces_ok == true then
         spaces.init({
             schema = 'Spaces',
             info = { enabled = true, include = {}, exclude = {} },
@@ -27,8 +27,15 @@ local function fragment()
             create = { enabled = true, include = {}, exclude = {} },
         })
     end
-    if spaces_ok == true then
-        service.init({ schema = 'Service' })
+    if service_ok == true then
+        service.init({
+            schema = 'Service',
+            box_cfg_get = { enabled = true, },
+            box_cfg_set = { enabled = true, },
+            fiber_info = { enabled = true, },
+            fiber_top = { enabled = true, },
+            fiber_cancel = { enabled = true, },
+        })
     end
 end
 
